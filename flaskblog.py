67bee2184fd6913091736563b2b961b344bf78d8
Jason Wallenfang <jason.wallenfang@gmail.com>
2020-03-24 00:11:43 -0500

    part4

--- flaskblog.py
+++ flaskblog.py
@@ -1,61 +1,100 @@
 # url_for: finds the exact location of routes for us, so we don't need to worry about it in the background
-# 
-from flask import Flask, escape, request, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+#
+# from flask import (Flask, escape, flash, redirect, render_template, request, url_for)
+from datetime import datetime
+
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from forms import LoginForm, RegistrationForm
 
 app = Flask(__name__)
 
 # python -> import secrets -> secrets.token_hex(16)
-app.config['SECRET_KEY'] = '57383f18a431c57cc60880218eff93c6'
+app.config["SECRET_KEY"] = "57383f18a431c57cc60880218eff93c6"
+# sqlite:/// is a relative path
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/site.db"
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship("Post", backref="author", lazy=True)
+
+    # magic method: how the object is printed when printed out
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    # magic method: how the object is printed when printed out
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}'')"
+
 
 posts = [
     {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
+        "author": "Corey Schafer",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "April 20, 2018",
     },
     {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "April 21, 2018",
+    },
 ]
 
 # route is used to navigate to different pages
-@app.route('/')
+@app.route("/")
 def greeting():
     name = request.args.get("name", "World")
-    return f'<h1>Hello, {escape(name)}!</h1><p>homepage</p>'
+    return f"<h1>Hello, {escape(name)}!</h1><p>homepage</p>"
 
-@app.route('/home')
+
+@app.route("/home")
 def home():
     return render_template("home.html", posts=posts)
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
     return render_template("about.html", title="About")
 
-@app.route('/register', methods=["GET", "POST"])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f"Account created for {form.username.data}!", "success")
-        return redirect(url_for('home'))
+        return redirect(url_for("home"))
     return render_template("register.html", title="Register", form=form)
 
-@app.route('/login', methods=["GET", "POST"])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         if form.email.data == "admin@blog.com" and form.password.data == "password":
             flash(f"You have been logged in!", "success")
-            return redirect(url_for('home'))
+            return redirect(url_for("home"))
         else:
             flash(f"Login failed. Please check username and password.", "danger")
     return render_template("login.html", title="Login", form=form)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
