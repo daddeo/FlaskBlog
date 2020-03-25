00a33b10137413ca7b7ee09e29976d8bd7fa0736
Jason Wallenfang <jason.wallenfang@gmail.com>
2020-03-24 20:40:49 -0500

    part6

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,6 +6,8 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -20,6 +22,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 # Invalid SQLite URL: sqlite://../C:\dev\src\Python\FlaskBlog\flaskblog\db/site.db
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://../db/site.db"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"  # the function, like url_for
+login_manager.login_message_category = "info"
 
 # cannot import at the top (circular import)
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,16 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User, Post
+
 
 class RegistrationForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired(), Length(min=2,max=20)])
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=20)]
+    )
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
-    confirmPassword = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo(fieldname='password')])
+    confirmPassword = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo(fieldname="password")]
+    )
     submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username already exists.")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email address already exists.")
+
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
+    remember = BooleanField("Remember Me")
     submit = SubmitField("Login")
