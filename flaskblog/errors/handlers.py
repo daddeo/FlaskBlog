5cd468e08d2ccae6a4bb9d796e2e1643d8645525
Jason Wallenfang <jason.wallenfang@gmail.com>
2020-04-02 00:10:54 -0500

    part12

---
+++ flaskblog/errors/handlers.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, render_template
+
+errors = Blueprint("errors", __name__)
+
+# return status code at the end (e.g. 404)
+# errors.errorhandler only works for the blueprint
+# errors.app_errorhandler works across the app
+
+
+@errors.app_errorhandler(404)
+def error_404(error):
+    return render_template("errors/404.html"), 404
+
+
+@errors.app_errorhandler(403)
+def error_403(error):
+    return render_template("errors/403.html"), 403
+
+
+@errors.app_errorhandler(500)
+def error_500(error):
+    return render_template("errors/500.html"), 500
